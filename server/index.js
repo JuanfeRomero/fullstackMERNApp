c7e26e6987eb305f930c63d1bddaa54f64f5bafb
JuanfeRomero <juanferomerocode@gmail.com>
2021-02-03 09:58:39 -0300

    frontend the form and the post request to call the backend and add the post to the mongo database

---
+++ server/index.js
@@ -0,0 +1,26 @@
+import dotenv from 'dotenv';
+dotenv.config();
+import express from 'express';
+import bodyParser from 'body-parser';
+import mongoose from 'mongoose';
+import cors from 'cors';
+
+import postRouter from './routes/postsRouter.js';
+
+const app = express();
+
+app.use(bodyParser.json({limit: "30mb", extended: true}))
+app.use(bodyParser.urlencoded({limit: "30mb", extended: true}))
+app.use(cors());
+
+// MONGO DB and PORT in the .env
+const CONNECTION_URL = process.env.MONGO_URI;
+const PORT = process.env.PORT || 5000;
+
+mongoose.connect(CONNECTION_URL, {useNewUrlParser: true, useUnifiedTopology: true})
+    .then(() => app.listen(PORT, () => console.log(`Server running on port: ${PORT}`)))
+    .catch((err) => console.error(err));
+
+mongoose.set('useFindAndModify', false);
+
+app.use('/posts', postRouter);
